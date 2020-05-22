64e2bd5e73986481be8e0a6c87eedc408a99d2bd
Rafael Goulart <faelplg@gmail.com>
2020-05-21 23:57:11 -0300

    First updates.

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from '../components/header';
+
+const About = props => (
+  <div style={{ color: `teal` }}>
+    <Header />
+    <p>Such wow. Very React.</p>
+  </div>
+)
+
+export default About
