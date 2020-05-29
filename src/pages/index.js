c95babb5e08350ec25ad67f39ee81ab2d83daf4a
Rafael Goulart <faelplg@gmail.com>
2020-05-29 01:25:55 -0300

    Learning to sort multiple markdown pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
 import React from "react"
 import Header from "../components/header"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default function Home() {
+export default function Home(props) {
+  console.log("props", props)
   return (
     <Layout>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world</p>
+      <Header headerText="Amazing Pandas Eating Things" />
+      <h4>{props.data.allMarkdownRemark.totalCount} Post(s)</h4>
       <img src="https://source.unsplash.com/random/400x200" alt="Random img." />
+      {props.data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
