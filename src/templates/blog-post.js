b250c90b5425a0a312ec1aa8020fdfd20eb7c79a
Rafael Goulart <faelplg@gmail.com>
2020-06-17 23:55:26 -0300

    Creating programmatically pages.

---
+++ src/templates/blog-post.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+function BlogPost() {
+  return (
+    <Layout>
+      <div>Hello blog post</div>
+    </Layout>
+  );
+}
+
+export default BlogPost;
